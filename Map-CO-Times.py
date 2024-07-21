579dcda3e4af0e2c604536b5801ea105a1d3dfbb
morganmayer <morgan.y.mayer@gmail.com>
2024-07-20 23:20:47 -0400

    wrote out neighborhoods csv

--- Map-CO-Times.py
+++ Map-CO-Times.py
@@ -2,18 +2,21 @@ import geopandas
 import plotly.express as px
 import seaborn as sns
 from Time_to_CO import *
-import geoplot
+#import geoplot
 import pandas as pd
-from shapely.geometry import Point, Polygon
-import matplotlib.pyplot as plt
+#from shapely.geometry import Point, Polygon
+#import matplotlib.pyplot as plt
 
 #path_to_data = get_path("nybb")
 #nyc_internet = gpd.read_file(path_to_data)
 
 df = clean_data()
 
+#what is the range across all of NYC?
 sns.histplot(data=df, x="diff_days")
 
+#averages across specific neighborhoods
+neighborhoods = df.groupby(['ntaName'])['diff_days'].mean().sort_values()
 
 
 #conversion form lat/long
@@ -38,3 +41,7 @@ fig = px.scatter_mapbox(df,
 fig.update_layout(mapbox_style="open-street-map")
 #fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
 fig.show()
+
+
+###write out
+neighborhoods.to_csv("Neighborhood Breakdown.csv")
