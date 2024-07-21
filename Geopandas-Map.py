196c67a2875a537cddf861e239d09ffd5ab19623
morganmayer <morgan.y.mayer@gmail.com>
2024-07-20 23:13:57 -0400

    finaly it's working!

--- Geopandas-Map.py
+++ Geopandas-Map.py
@@ -11,13 +11,14 @@ import plotly.express as px
 #kings_county_map.plot()
 
 path_to_data = get_path("nybb")
-nyc = gpd.read_file(path_to_data)
+nyc_internet = gpd.read_file(path_to_data)
 
-nyc = pd.read_csv("nybb_20240720.csv")
+nyc = pd.read_csv("nybb_20240720.csv").rename(columns = {"the_geom" : "geometry"} )
+nyc.columns
 
-path_boroughs = get_path("nyc_boroughs")
+#path_boroughs = get_path("nyc_boroughs")
 
-boroughs = geopandas.read_file(geoplot.datasets.get_path("nyc_boroughs"))
+#boroughs = geopandas.read_file(geoplot.datasets.get_path("nyc_boroughs"))
 
 #gdf["area"] = gdf.area
 
@@ -40,7 +41,7 @@ gdf[~gdf["lat"].str.contains("POINT EMPTY")]
 #world = gpd.read_file(geodatasets.data.naturalearth.land['url'])
 
 
-gdf.plot(ax=nyc.plot(figsize=(10, 6)), marker='o', color='red', markersize=15)
+gdf.plot(ax=nyc_internet.plot(figsize=(10, 6)), marker='o', color='red', markersize=15)
 
 # Create scatter map
 fig = px.scatter_geo(df, lat='latitude', lon='longitude', color='diff_days',
@@ -72,3 +73,56 @@ geo_df[geo_df['diff_days'] >= 40].plot(ax=ax,
                                        marker='^',
                                        label='Pos')
 plt.legend(prop={'size':15})
+
+
+
+# creating a geometry column
+geometry = [Point(xy) for xy in zip(df['longitude'], df['latitude'])]
+# Coordinate reference system : WGS84
+crs = {'init': 'epsg:4326'}
+# Creating a Geographic data frame
+gdf = gpd.GeoDataFrame(df, crs=crs, geometry=geometry)
+gdf.plot(marker='*', markersize=0.2)
+
+from pyproj import Proj
+def rule(row):
+    p = Proj(proj='utm',zone=10,ellps='WGS84', preserve_units=False)
+    x,y = p(row["longitude"], row["latitude"])
+    return pd.Series({"X": x , "Y": y})
+
+
+My_data = df.merge(df.apply(rule, axis=1), left_index= True, right_index= True)
+
+geometry = [Point(xy) for xy in zip(My_data['X'], My_data['Y'])]
+
+
+
+
+# Creating a Geographic data frame
+gdf = gpd.GeoDataFrame(My_data, crs=crs, geometry=geometry)
+gdf.plot(marker='*', markersize=0.2)
+
+
+crs={'init':'epsg:4326'}
+
+#boroughs = geopandas.read_file(geoplot.datasets.get_path('nyc_boroughs'))
+
+geo_df=geopandas.GeoDataFrame(df,crs=crs,geometry=geopandas.points_from_xy(df["longitude"], df["latitude"]))
+
+color_scale = [(0, 'yellow'), (1,'red')]
+
+fig = px.scatter_mapbox(df,
+                        lat="latitude",
+                        lon="longitude",
+                        hover_name="STREET NAME",
+                        hover_data=["HOUSE NO"],
+                        color="diff_days",
+                        color_continuous_scale=color_scale,
+                        size="diff_days",
+                        zoom=8,
+                        height=800,
+                        width=800)
+
+fig.update_layout(mapbox_style="open-street-map")
+fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
+fig.show()
--- Map-CO-Times.py
+++ Map-CO-Times.py
@@ -1,22 +1,40 @@
 import geopandas
+import plotly.express as px
+import seaborn as sns
+from Time_to_CO import *
 import geoplot
 import pandas as pd
 from shapely.geometry import Point, Polygon
 import matplotlib.pyplot as plt
 
-store_locDF=pd.read_csv('stores.csv')
-cols_to_keep=['store_longitude','store_latitude']
+#path_to_data = get_path("nybb")
+#nyc_internet = gpd.read_file(path_to_data)
 
+df = clean_data()
+
+sns.histplot(data=df, x="diff_days")
+
+
+
+#conversion form lat/long
 crs={'init':'epsg:4326'}
+geo_df=geopandas.GeoDataFrame(df,crs=crs,geometry=geopandas.points_from_xy(df["longitude"], df["latitude"]))
 
-boroughs = geopandas.read_file(geoplot.datasets.get_path('nyc_boroughs'))
+#vis settings
+color_scale = [(0, 'FC6955'), (1,'AF0038')]
+fig = px.scatter_mapbox(df,
+                        lat="latitude",
+                        lon="longitude",
+                        hover_name="STREET NAME",
+                        hover_data=["HOUSE NO"],
+                        color="diff_days",
+                        #color_continuous_scale=color_scale,
+                        color_continuous_scale=px.colors.sequential.Viridis_r,
+                        size="diff_days",
+                        zoom=8,
+                        height=800,
+                        width=800)
 
-geo_df=geopandas.GeoDataFrame(store_locDF,crs=crs,geometry=geopandas.points_from_xy(store_locDF["store_longitude"], store_locDF["store_latitude"]))
-
-
-
-fig,ax=plt.subplots(figsize=(15,15))
-boroughs.plot(ax=ax,alpha=0.4,color="grey")
-geo_df[geo_df['active_yn']=="Y"].plot(ax=ax,markersize=200, alpha=0.4,color="green", label="Active Store")
-geo_df[geo_df['active_yn']=="N"].plot(ax=ax,markersize=200, alpha=0.4,color="red", label="Inactive Stores")
-plt.legend()
+fig.update_layout(mapbox_style="open-street-map")
+#fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
+fig.show()
