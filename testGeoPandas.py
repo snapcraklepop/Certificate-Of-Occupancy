02101aa0107a9710f5eb896a0ab1234ba0ac2efd
Morgan Mayer <166623808+snapcraklepop@users.noreply.github.com>
2024-07-20 23:21:53 -0400

    Delete testGeoPandas.py

--- testGeoPandas.py
+++
@@ -1,4 +0,0 @@
-from uszipcode import SearchEnginesearch
-
-SearchEnginesearch.SearchEngine(simple_zipcode = False)
-metro_nyc = search.by_coordinates(lat = 40.73, lng = -73.99, radius = 75, returns = 500)
